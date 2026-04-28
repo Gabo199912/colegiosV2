7b84228613758f5f9a9de1469046fefe73f224da
angel <angelvasquez199912@gmail.com>
2026-04-27 20:03:22 -0600

    se empieza a agregar la parte de los grados, para poder listar crear y agregar con los alumnos el grado.

---
+++ src/main/java/com/educacion/inedcuchilla/controlador/GradoControlador.java
@@ -0,0 +1,45 @@
+package com.educacion.inedcuchilla.controlador;
+
+import com.educacion.inedcuchilla.modelo.GradoModelo;
+import com.educacion.inedcuchilla.servicio.GradoServicio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/grado")
+public class GradoControlador {
+    Map<String, Object> respuesta = new HashMap<>();
+    private final GradoServicio gradoServicio;
+
+    public GradoControlador(GradoServicio gradoServicio) {
+        this.gradoServicio = gradoServicio;
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<?> listarGrados(){
+        List<GradoModelo> grados = gradoServicio.listarGrados();
+
+        if (grados.isEmpty()){
+            Map<String, Object> respuesta = new HashMap<>();
+            respuesta.put("mensaje", "No hay grados registrados");
+            return ResponseEntity.status(404).body(respuesta);
+        }
+
+        return ResponseEntity.ok(grados);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<?> crearGrado(@RequestBody GradoModelo gradoModelo){
+
+        try {
+            gradoServicio.guardarGrado(gradoModelo);
+        }catch (RuntimeException e){
+            return ResponseEntity.status(409).body(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/com/educacion/inedcuchilla/servicio/GradoServicio.java
+++ src/main/java/com/educacion/inedcuchilla/servicio/GradoServicio.java
@@ -2,12 +2,16 @@ package com.educacion.inedcuchilla.servicio;
 
 import com.educacion.inedcuchilla.modelo.GradoModelo;
 import com.educacion.inedcuchilla.repositorio.GradoRepositorio;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class GradoServicio {
+
     private final GradoRepositorio gradoRepositorio;
 
     public GradoServicio(GradoRepositorio gradoRepositorio) {
@@ -21,5 +25,31 @@ public class GradoServicio {
     public List<GradoModelo> listarGrados(){
         return gradoRepositorio.findAll();
     }
+
+    public Map<String, Object> guardarGrado(GradoModelo gradoModelo){
+        Map<String, Object> respuesta = new HashMap<>();
+
+        char seccion = gradoModelo.getSeccion();
+
+        if (gradoModelo.getNombreGrado().isEmpty()){
+            respuesta.put("mensaje", "El nombre del grado no puede estar vacio");
+            return respuesta;
+        }
+
+        if (gradoModelo.getEspecialidad().isEmpty()){
+            respuesta.put("mensaje", "La especialidad del grado no puede estar vacia");
+            return respuesta;
+        }
+
+
+
+        gradoRepositorio.save(gradoModelo);
+
+        respuesta.put("RESPUESTA", "se guardo el grado correctactamente");
+
+        return respuesta;
+    }
+
+
 
 }
