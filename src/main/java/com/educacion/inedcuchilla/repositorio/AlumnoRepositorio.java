4df28c384eba485f02f6a64e4c63be8c02bcf8dc
Gabo <angelvasquez199912@icloud.com>
2026-07-19 12:38:17 -0600

    se termino la parte de crear alumno y convertir a alumno un usuario, en otras palabras agregarle el rol de alumno.
    aun falta la parte de asignarle una inscripción.
    
    esta parte esta mas hecha para que profesores o directores puedan asignarse ese rol y ver todo lo de un alumno tambien, no para que se cree un alumno, ya que se crearia asignandole de una vez una inscripción.

--- src/main/java/com/educacion/inedcuchilla/repositorio/AlumnoRepositorio.java
+++ src/main/java/com/educacion/inedcuchilla/repositorio/AlumnoRepositorio.java
@@ -4,6 +4,7 @@ import com.educacion.inedcuchilla.modelo.AlumnoModelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AlumnoRepositorio extends JpaRepository<AlumnoModelo, Integer> {
-}
+public interface AlumnoRepositorio extends JpaRepository<AlumnoModelo, Integer> { }
